208904ee6ff05d1972c671e992bc7b9d188f7795
gyoomi <gyoomi0709@foxmail.com>
2019-04-11 18:06:34 +0800

    add:modify code

--- src/main/java/com/concurrent/blockingqueue/ArrayBlockingQueue01.java
+++ src/main/java/com/concurrent/blockingqueue/ArrayBlockingQueue01.java
@@ -18,7 +18,7 @@ import java.util.concurrent.BlockingQueue;
 public class ArrayBlockingQueue01 {
 
     public static void main(String[] args) {
-        BlockingQueue<Integer> bq = new ArrayBlockingQueue(10);
+        final BlockingQueue<Integer> bq = new ArrayBlockingQueue(10);
         Runnable produce01 = new Runnable(){
             int i = 0;
             @Override
@@ -34,27 +34,33 @@ public class ArrayBlockingQueue01 {
                 }
             }
         };
-        Runnable customer01 = () -> {
-            for (;;) {
-                try {
-                    System.out.println("消费者01消费了一个：" + bq.take());
-                    Thread.sleep(600);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-        Runnable customer02 = () -> {
-            for (;;) {
-                try {
-                    System.out.println("消费者02消费了一个：" + bq.take());
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+        Runnable customer01 = new Runnable() {
+            @Override
+            public void run() {
+                for (;;) {
+                    try {
+                        System.out.println("消费者01消费了一个：" + bq.take());
+                        Thread.sleep(600);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         };
 
+        Runnable customer02 = new Runnable() {
+            @Override
+            public void run() {
+                for (;;) {
+                    try {
+                        System.out.println("消费者02消费了一个：" + bq.take());
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
         Thread t01 = new Thread(customer01);
         Thread t02 = new Thread(customer02);
         Thread t03 = new Thread(produce01);
